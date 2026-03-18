6ac2b17015606181d9f0b9a0031eaf46b7f98b46
PedroBueno <pedrovieirabueno@icloud.com>
2026-03-18 13:19:33 -0300

    meu primeiro commit

---
+++ condicao-complexa-END/script.js
@@ -0,0 +1,8 @@
+let idade = Number(prompt("insira sua idade"));
+let temCNH = prompt("possui CNH ?(sim ou não)");
+
+if (idade >= 18 && temCNH= ==="sim") {
+        alert("pode dirigir");
+} else {
+        alert("não pode dirigir");
+}
